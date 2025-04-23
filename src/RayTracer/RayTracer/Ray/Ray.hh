01fd2b24c4e56db4655ba913a2c360da6f1c68ed
Alan CUNIN <alan.cunin@epitech.eu>
2025-04-24 00:27:20 +0200

    feat(RAYT-1): Create the Ray class

---
+++ src/RayTracer/RayTracer/Ray/Ray.hh
@@ -0,0 +1,32 @@
+/*
+** EPITECH PROJECT, 2025
+** Raytracer
+** File description:
+** Ray
+*/
+
+#pragma once
+
+#include "Math/Point3D/Point3D.hh"
+#include "Math/Vector3D/Vector3D.hh"
+
+namespace RayTracer
+{
+    class Ray
+    {
+        public:
+            Ray();
+            Ray(Math::Point3D& origin, Math::Vector3D& direction);
+            ~Ray() = default;
+
+            Math::Point3D getOrigin(void) const;
+            Math::Vector3D getDirection(void) const;
+
+            void setOrigin(const Math::Point3D& origin);
+            void setDirection(const Math::Vector3D& direction);
+
+        private:
+            Math::Point3D _origin;
+            Math::Vector3D _direction;
+    };
+}
