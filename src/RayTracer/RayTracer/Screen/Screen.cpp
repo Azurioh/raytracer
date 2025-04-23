c3540ee3c48dce27c4236f0093297878024c7556
Alan CUNIN <alan.cunin@epitech.eu>
2025-04-24 01:35:45 +0200

    feat(RAYT-1): Add missing functions in the Screen class

--- src/RayTracer/RayTracer/Screen/Screen.cpp
+++ src/RayTracer/RayTracer/Screen/Screen.cpp
@@ -7,22 +7,51 @@
 
 #include "Screen.hh"
 
-RayTracer::Camera::Screen::Screen()
+RayTracer::Screen::Screen()
 {
 }
 
-RayTracer::Camera::Screen::Screen(const Math::Point3D& origin, const Math::Vector3D& bottomSide, const Math::Vector3D& leftSide)
+RayTracer::Screen::Screen(const Math::Point3D& origin, const Math::Vector3D& bottomSide, const Math::Vector3D& leftSide): _origin(origin)
 {
-    _origin = origin;
     _bottomSide = bottomSide;
     _leftSide = leftSide;
 }
 
-RayTracer::Camera::Screen::~Screen()
+RayTracer::Screen::~Screen()
 {
 }
 
-Math::Point3D RayTracer::Camera::Screen::pointAt(double u, double v)
+Math::Point3D RayTracer::Screen::getOrigin(void) const
+{
+    return _origin;
+}
+
+Math::Vector3D RayTracer::Screen::getBottomSide(void) const
+{
+	return _bottomSide;
+}
+
+Math::Vector3D RayTracer::Screen::getLeftSide(void) const
+{
+	return _leftSide;
+}
+
+void RayTracer::Screen::setOrigin(const Math::Point3D& point)
+{
+	_origin = point;
+}
+
+void RayTracer::Screen::setBottomSide(const Math::Vector3D& vector)
+{
+	_bottomSide = vector;
+}
+
+void RayTracer::Screen::setLeftSide(const Math::Vector3D& vector)
+{
+	_leftSide = vector;
+}
+
+Math::Point3D RayTracer::Screen::pointAt(double u, double v)
 {
     Math::Point3D result = _origin;
     Math::Vector3D movement(_bottomSide.length() * u, _leftSide.length() * v, 0);
