255199141c2d9276cec10bb0503f274d191a35dd
Florianperidy <florian.peridy@epitech.eu>
2025-05-07 14:57:48 +0200

    Feat: pushing modified main to create parser class

--- src/RayTracer/main.cpp
+++ src/RayTracer/main.cpp
@@ -6,13 +6,17 @@
 */
 
 #include "RayTracer/Core/Core.hh"
+#include "../Parser/Parser.hh"
 
 #include <cstdint>
 
-std::int32_t main()
+std::int32_t main(int ac, char **av)
 {
     RayTracer::Core core;
+    RayTracer::Parser parser;
 
+    (void)ac;
+    parser.parseFile(av[1]);
     core.start();
     return 0;
 }
